69870d5711687a363c81e35e3ebf332aef1c5fd2
djw <service@t-firefly.com>
2018-05-23 11:16:18 +0800

    Exclude include, private for building doc

--- zh_CN/conf.py
+++ zh_CN/conf.py
@@ -83,7 +83,8 @@ html_search_language = 'zh'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sphinx-markdown', 'README.md', 'share']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sphinx-markdown', 'README.md', 'include', 'private', 'share']
+
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
